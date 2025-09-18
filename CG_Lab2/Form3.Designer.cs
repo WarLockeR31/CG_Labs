933505b0a24b392d87a2100b1f6dc144a6a2bde8
WarLockeR31 <roman.borisenko2016@gmail.com>
2025-09-18 15:09:17 +0300

    Feat: Complete Task 3
    +HSV adjustment logic and UI

--- CG_Lab2/Form3.Designer.cs
+++ CG_Lab2/Form3.Designer.cs
@@ -28,12 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form3";
+            tbHue = new TrackBar();
+            lblHue = new Label();
+            lblSat = new Label();
+            tbSat = new TrackBar();
+            lblVal = new Label();
+            tbVal = new TrackBar();
+            pb = new PictureBox();
+            btnOpen = new Button();
+            btnSave = new Button();
+            btnReset = new Button();
+            ((System.ComponentModel.ISupportInitialize)tbHue).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)tbSat).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)tbVal).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pb).BeginInit();
+            SuspendLayout();
+            // 
+            // tbHue
+            // 
+            tbHue.Location = new Point(497, 84);
+            tbHue.Name = "tbHue";
+            tbHue.Size = new Size(270, 45);
+            tbHue.TabIndex = 3;
+            tbHue.TickStyle = TickStyle.None;
+            tbHue.Scroll += tbHue_Scroll;
+            // 
+            // lblHue
+            // 
+            lblHue.BorderStyle = BorderStyle.FixedSingle;
+            lblHue.Font = new Font("Segoe UI", 12F);
+            lblHue.Location = new Point(587, 57);
+            lblHue.Name = "lblHue";
+            lblHue.Size = new Size(93, 23);
+            lblHue.TabIndex = 4;
+            lblHue.Text = "Hue:";
+            lblHue.Click += lblHue_Click;
+            // 
+            // lblSat
+            // 
+            lblSat.BorderStyle = BorderStyle.FixedSingle;
+            lblSat.Font = new Font("Segoe UI", 12F);
+            lblSat.Location = new Point(587, 131);
+            lblSat.Name = "lblSat";
+            lblSat.Size = new Size(93, 23);
+            lblSat.TabIndex = 6;
+            lblSat.Text = "Sat:";
+            // 
+            // tbSat
+            // 
+            tbSat.Location = new Point(497, 161);
+            tbSat.Name = "tbSat";
+            tbSat.Size = new Size(270, 45);
+            tbSat.TabIndex = 5;
+            tbSat.TickStyle = TickStyle.None;
+            tbSat.Scroll += tbSat_Scroll;
+            // 
+            // lblVal
+            // 
+            lblVal.BorderStyle = BorderStyle.FixedSingle;
+            lblVal.Font = new Font("Segoe UI", 12F);
+            lblVal.Location = new Point(587, 208);
+            lblVal.Name = "lblVal";
+            lblVal.Size = new Size(93, 23);
+            lblVal.TabIndex = 8;
+            lblVal.Text = "Val:";
+            lblVal.Click += lblVal_Click;
+            // 
+            // tbVal
+            // 
+            tbVal.Location = new Point(497, 235);
+            tbVal.Name = "tbVal";
+            tbVal.Size = new Size(270, 45);
+            tbVal.TabIndex = 7;
+            tbVal.TickStyle = TickStyle.None;
+            tbVal.Scroll += tbVal_Scroll;
+            // 
+            // pb
+            // 
+            pb.Location = new Point(12, 58);
+            pb.Name = "pb";
+            pb.Size = new Size(456, 380);
+            pb.TabIndex = 9;
+            pb.TabStop = false;
+            // 
+            // btnOpen
+            // 
+            btnOpen.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnOpen.Location = new Point(497, 322);
+            btnOpen.Name = "btnOpen";
+            btnOpen.Size = new Size(80, 61);
+            btnOpen.TabIndex = 10;
+            btnOpen.Text = "Open";
+            btnOpen.UseVisualStyleBackColor = true;
+            btnOpen.Click += btnOpen_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnSave.Location = new Point(592, 322);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(80, 61);
+            btnSave.TabIndex = 11;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // btnReset
+            // 
+            btnReset.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnReset.Location = new Point(687, 322);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(80, 61);
+            btnReset.TabIndex = 12;
+            btnReset.Text = "Reset";
+            btnReset.UseVisualStyleBackColor = true;
+            btnReset.Click += btnReset_Click;
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnReset);
+            Controls.Add(btnSave);
+            Controls.Add(btnOpen);
+            Controls.Add(pb);
+            Controls.Add(lblVal);
+            Controls.Add(tbVal);
+            Controls.Add(lblSat);
+            Controls.Add(tbSat);
+            Controls.Add(lblHue);
+            Controls.Add(tbHue);
+            Name = "Form3";
+            Text = "Form3";
+            Controls.SetChildIndex(tbHue, 0);
+            Controls.SetChildIndex(lblHue, 0);
+            Controls.SetChildIndex(tbSat, 0);
+            Controls.SetChildIndex(lblSat, 0);
+            Controls.SetChildIndex(tbVal, 0);
+            Controls.SetChildIndex(lblVal, 0);
+            Controls.SetChildIndex(pb, 0);
+            Controls.SetChildIndex(btnOpen, 0);
+            Controls.SetChildIndex(btnSave, 0);
+            Controls.SetChildIndex(btnReset, 0);
+            ((System.ComponentModel.ISupportInitialize)tbHue).EndInit();
+            ((System.ComponentModel.ISupportInitialize)tbSat).EndInit();
+            ((System.ComponentModel.ISupportInitialize)tbVal).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pb).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TrackBar tbHue;
+        private Label lblHue;
+        private Label lblSat;
+        private TrackBar tbSat;
+        private Label lblVal;
+        private TrackBar tbVal;
+        private PictureBox pb;
+        private Button btnOpen;
+        private Button btnSave;
+        private Button btnReset;
     }
 }
--- CG_Lab2/Form3.cs
+++ CG_Lab2/Form3.cs
@@ -1,20 +1,180 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Windows.Forms;
+using FastBitmap; // для Select/ForEach
 
 namespace CG_Lab2
 {
-    public partial class Form3: SwitchableForm
+    public partial class Form3 : SwitchableForm
     {
+        private Bitmap _original;
+        private Bitmap _preview;
+
         public Form3()
         {
             InitializeComponent();
+            InitUiDefaults();
+        }
+
+        private void InitUiDefaults()
+        {
+            tbHue.Minimum = -180; tbHue.Maximum = 180; tbHue.Value = 0;
+            tbSat.Minimum = 0; tbSat.Maximum = 200; tbSat.Value = 100;
+            tbVal.Minimum = 0; tbVal.Maximum = 200; tbVal.Value = 100;
+            UpdateLabels();
+            ToggleUi(false);
+        }
+
+        #region Buttons
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using var ofd = new OpenFileDialog
+            {
+                Filter = "Изображения|*.png;*.jpg;*.jpeg;*.bmp;*.gif|Все файлы|*.*"
+            };
+            if (ofd.ShowDialog(this) != DialogResult.OK) return;
+
+            _original?.Dispose();
+            _preview?.Dispose();
+
+            _original = new Bitmap(ofd.FileName);
+            _preview = (Bitmap)_original.Clone();
+            pb.Image = _preview;
+
+            ToggleUi(true);
+            ApplyAndShow();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (_preview == null) return;
+
+            using var sfd = new SaveFileDialog
+            {
+                Filter = "PNG|*.png|JPEG|*.jpg;*.jpeg|BMP|*.bmp",
+                FileName = "result.png"
+            };
+            if (sfd.ShowDialog(this) != DialogResult.OK) return;
+
+            var fmt = ImageFormat.Png;
+            var ext = Path.GetExtension(sfd.FileName).ToLowerInvariant();
+            if (ext == ".jpg" || ext == ".jpeg") fmt = ImageFormat.Jpeg;
+            else if (ext == ".bmp") fmt = ImageFormat.Bmp;
+
+            _preview.Save(sfd.FileName, fmt);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            tbHue.Value = 0; tbSat.Value = 100; tbVal.Value = 100;
+            UpdateLabels();
+            ApplyAndShow();
+        }
+        #endregion
+
+        #region TrackBars
+        private void tbHue_Scroll(object sender, EventArgs e) { UpdateLabels(); ApplyAndShow(); }
+        private void tbSat_Scroll(object sender, EventArgs e) { UpdateLabels(); ApplyAndShow(); }
+        private void tbVal_Scroll(object sender, EventArgs e) { UpdateLabels(); ApplyAndShow(); }
+        #endregion
+
+        private void UpdateLabels()
+        {
+            lblHue.Text = $"Hue: {tbHue.Value}°";
+            lblSat.Text = $"Sat: {tbSat.Value}%";
+            lblVal.Text = $"Val: {tbVal.Value}%";
+        }
+
+        private void ToggleUi(bool on)
+        {
+            tbHue.Enabled = tbSat.Enabled = tbVal.Enabled = btnSave.Enabled = btnReset.Enabled = on;
+        }
+
+        private void ApplyAndShow()
+        {
+            if (_original == null) return;
+
+            int hueDelta = tbHue.Value;          // -180..+180 градусов
+            double sMul = tbSat.Value / 100.0;  // 0..2.0
+            double vMul = tbVal.Value / 100.0;  // 0..2.0
+
+            _preview?.Dispose();
+            _preview = _original.Select(c =>
+            {
+                double h, s, v;
+                RgbToHsv(c, out h, out s, out v);
+
+                h = (h + hueDelta) % 360.0; if (h < 0) h += 360.0;
+                s = Clamp01(s * sMul);
+                v = Clamp01(v * vMul);
+
+                return HsvToRgb(h, s, v, c.A);
+            });
+
+            pb.Image = _preview;
+        }
+
+        #region RGB<->HSV 
+        private static void RgbToHsv(Color c, out double h, out double s, out double v)
+        {
+            double r = c.R / 255.0, g = c.G / 255.0, b = c.B / 255.0;
+            double max = Math.Max(r, Math.Max(g, b));
+            double min = Math.Min(r, Math.Min(g, b));
+            double delta = max - min;
+
+            // Hue
+            if (delta == 0) h = 0;
+            else if (max == r) h = 60.0 * (((g - b) / delta) % 6.0);
+            else if (max == g) h = 60.0 * (((b - r) / delta) + 2.0);
+            else h = 60.0 * (((r - g) / delta) + 4.0);
+            if (h < 0) h += 360.0;
+
+            // Saturation & Value
+            v = max;
+            s = max == 0 ? 0 : delta / max;
+        }
+
+        private static Color HsvToRgb(double h, double s, double v, byte a = 255)
+        {
+            double c = v * s;
+            double hp = h / 60.0;
+            double x = c * (1.0 - Math.Abs(hp % 2.0 - 1.0));
+
+            double r1 = 0, g1 = 0, b1 = 0;
+            if (0 <= hp && hp < 1) { r1 = c; g1 = x; b1 = 0; }
+            else if (1 <= hp && hp < 2) { r1 = x; g1 = c; b1 = 0; }
+            else if (2 <= hp && hp < 3) { r1 = 0; g1 = c; b1 = x; }
+            else if (3 <= hp && hp < 4) { r1 = 0; g1 = x; b1 = c; }
+            else if (4 <= hp && hp < 5) { r1 = x; g1 = 0; b1 = c; }
+            else { r1 = c; g1 = 0; b1 = x; }
+
+            double m = v - c;
+            byte R = (byte)Math.Round(Clamp01(r1 + m) * 255.0);
+            byte G = (byte)Math.Round(Clamp01(g1 + m) * 255.0);
+            byte B = (byte)Math.Round(Clamp01(b1 + m) * 255.0);
+            return Color.FromArgb(a, R, G, B);
+        }
+        #endregion
+
+        private static double Clamp01(double x) => x < 0 ? 0 : (x > 1 ? 1 : x);
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            _preview?.Dispose();
+            _original?.Dispose();
+            base.OnFormClosed(e);
+        }
+
+        private void lblHue_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblVal_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
