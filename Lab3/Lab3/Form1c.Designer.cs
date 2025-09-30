bf914c7db6aea46cc0db1aad883d7eba97be5327
WarLockeR31 <roman.borisenko2016@gmail.com>
2025-10-01 00:30:20 +0300

    Feat: Add PixelPictureBox and improve contour drawing

--- Lab3/Lab3/Form1c.Designer.cs
+++ Lab3/Lab3/Form1c.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1c));
-            pictureBox1 = new PictureBox();
             btnOpen = new Button();
+            pictureBox1 = new PixelPictureBox();
+            btnClear = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -39,46 +39,59 @@
             lblTask.Size = new Size(118, 27);
             lblTask.Text = "Задание 1в";
             // 
+            // btnOpen
+            // 
+            btnOpen.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnOpen.Location = new Point(679, 387);
+            btnOpen.Name = "btnOpen";
+            btnOpen.Size = new Size(91, 42);
+            btnOpen.TabIndex = 7;
+            btnOpen.Text = "Открыть";
+            btnOpen.UseVisualStyleBackColor = true;
+            // 
             // pictureBox1
             // 
-            pictureBox1.InitialImage = (Image)resources.GetObject("pictureBox1.InitialImage");
+            pictureBox1.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
             pictureBox1.Location = new Point(12, 58);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(500, 380);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox1.TabIndex = 6;
+            pictureBox1.TabIndex = 8;
             pictureBox1.TabStop = false;
             // 
-            // btnOpen
+            // btnClear
             // 
-            btnOpen.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            btnOpen.Location = new Point(634, 396);
-            btnOpen.Name = "btnOpen";
-            btnOpen.Size = new Size(85, 42);
-            btnOpen.TabIndex = 7;
-            btnOpen.Text = "Открыть";
-            btnOpen.UseVisualStyleBackColor = true;
+            btnClear.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnClear.Location = new Point(559, 387);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(91, 42);
+            btnClear.TabIndex = 9;
+            btnClear.Text = "Очистить";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
             // 
             // Form1c
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(btnOpen);
+            Controls.Add(btnClear);
             Controls.Add(pictureBox1);
+            Controls.Add(btnOpen);
             Name = "Form1c";
             Text = "Form1c";
+            Controls.SetChildIndex(btnOpen, 0);
             Controls.SetChildIndex(lblTask, 0);
             Controls.SetChildIndex(pictureBox1, 0);
-            Controls.SetChildIndex(btnOpen, 0);
+            Controls.SetChildIndex(btnClear, 0);
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private PictureBox pictureBox1;
         private Button btnOpen;
+        private PixelPictureBox pictureBox1;
+        private Button btnClear;
     }
 }
