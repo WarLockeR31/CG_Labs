738adf1d0b360b1450a4ab2f44c81975f8b82ee3
Barashkofff <115090287+Barashkofff@users.noreply.github.com>
2025-10-22 01:39:10 +0300

    lab5

--- Lab5/Lab5/Form2.Designer.cs
+++ Lab5/Lab5/Form2.Designer.cs
@@ -28,23 +28,199 @@
         /// </summary>
         private void InitializeComponent()
         {
-            SuspendLayout();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.roughnessTrackBar = new System.Windows.Forms.TrackBar();
+            this.snowLevelTrackBar = new System.Windows.Forms.TrackBar();
+            this.sizeTrackBar = new System.Windows.Forms.TrackBar();
+            this.seedNumeric = new System.Windows.Forms.NumericUpDown();
+            this.stepComboBox = new System.Windows.Forms.ComboBox();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.roughnessLabel = new System.Windows.Forms.Label();
+            this.snowLevelLabel = new System.Windows.Forms.Label();
+            this.sizeLabel = new System.Windows.Forms.Label();
+            this.seedLabel = new System.Windows.Forms.Label();
+            this.stepLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roughnessTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.snowLevelTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sizeTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seedNumeric)).BeginInit();
+            this.SuspendLayout();
             // 
-            // lblTask
+            // pictureBox
             // 
-            lblTask.Text = "Задание 2";
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(12, 80);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(500, 400);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // roughnessTrackBar
+            // 
+            this.roughnessTrackBar.Location = new System.Drawing.Point(530, 105);
+            this.roughnessTrackBar.Maximum = 100;
+            this.roughnessTrackBar.Minimum = 1;
+            this.roughnessTrackBar.Name = "roughnessTrackBar";
+            this.roughnessTrackBar.Size = new System.Drawing.Size(250, 45);
+            this.roughnessTrackBar.TabIndex = 1;
+            this.roughnessTrackBar.TickFrequency = 10;
+            this.roughnessTrackBar.Value = 50;
+            // 
+            // snowLevelTrackBar
+            // 
+            this.snowLevelTrackBar.Location = new System.Drawing.Point(530, 200);
+            this.snowLevelTrackBar.Maximum = 100;
+            this.snowLevelTrackBar.Name = "snowLevelTrackBar";
+            this.snowLevelTrackBar.Size = new System.Drawing.Size(250, 45);
+            this.snowLevelTrackBar.TabIndex = 2;
+            this.snowLevelTrackBar.TickFrequency = 10;
+            this.snowLevelTrackBar.Value = 70;
+            // 
+            // sizeTrackBar
+            // 
+            this.sizeTrackBar.Location = new System.Drawing.Point(530, 295);
+            this.sizeTrackBar.Maximum = 10;
+            this.sizeTrackBar.Minimum = 5;
+            this.sizeTrackBar.Name = "sizeTrackBar";
+            this.sizeTrackBar.Size = new System.Drawing.Size(250, 45);
+            this.sizeTrackBar.TabIndex = 3;
+            this.sizeTrackBar.TickFrequency = 1;
+            this.sizeTrackBar.Value = 7;
+            // 
+            // seedNumeric
+            // 
+            this.seedNumeric.Location = new System.Drawing.Point(530, 375);
+            this.seedNumeric.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.seedNumeric.Name = "seedNumeric";
+            this.seedNumeric.Size = new System.Drawing.Size(120, 22);
+            this.seedNumeric.TabIndex = 4;
+            // 
+            // stepComboBox
+            // 
+            this.stepComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.stepComboBox.FormattingEnabled = true;
+            this.stepComboBox.Location = new System.Drawing.Point(530, 420);
+            this.stepComboBox.Name = "stepComboBox";
+            this.stepComboBox.Size = new System.Drawing.Size(250, 24);
+            this.stepComboBox.TabIndex = 5;
+            // 
+            // generateButton
+            // 
+            this.generateButton.BackColor = System.Drawing.Color.LightBlue;
+            this.generateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.generateButton.Location = new System.Drawing.Point(530, 465);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(250, 35);
+            this.generateButton.TabIndex = 6;
+            this.generateButton.Text = "Сгенерировать горный массив";
+            this.generateButton.UseVisualStyleBackColor = false;
+            // 
+            // roughnessLabel
+            // 
+            this.roughnessLabel.AutoSize = true;
+            this.roughnessLabel.Location = new System.Drawing.Point(527, 80);
+            this.roughnessLabel.Name = "roughnessLabel";
+            this.roughnessLabel.Size = new System.Drawing.Size(137, 16);
+            this.roughnessLabel.TabIndex = 7;
+            this.roughnessLabel.Text = "Шероховатость: 0.50";
+            // 
+            // snowLevelLabel
+            // 
+            this.snowLevelLabel.AutoSize = true;
+            this.snowLevelLabel.Location = new System.Drawing.Point(527, 175);
+            this.snowLevelLabel.Name = "snowLevelLabel";
+            this.snowLevelLabel.Size = new System.Drawing.Size(125, 16);
+            this.snowLevelLabel.TabIndex = 8;
+            this.snowLevelLabel.Text = "Уровень снега: 0.70";
+            // 
+            // sizeLabel
+            // 
+            this.sizeLabel.AutoSize = true;
+            this.sizeLabel.Location = new System.Drawing.Point(527, 270);
+            this.sizeLabel.Name = "sizeLabel";
+            this.sizeLabel.Size = new System.Drawing.Size(102, 16);
+            this.sizeLabel.TabIndex = 9;
+            this.sizeLabel.Text = "Размер: 129x129";
+            // 
+            // seedLabel
+            // 
+            this.seedLabel.AutoSize = true;
+            this.seedLabel.Location = new System.Drawing.Point(527, 355);
+            this.seedLabel.Name = "seedLabel";
+            this.seedLabel.Size = new System.Drawing.Size(42, 16);
+            this.seedLabel.TabIndex = 10;
+            this.seedLabel.Text = "Seed:";
+            // 
+            // stepLabel
+            // 
+            this.stepLabel.AutoSize = true;
+            this.stepLabel.Location = new System.Drawing.Point(527, 400);
+            this.stepLabel.Name = "stepLabel";
+            this.stepLabel.Size = new System.Drawing.Size(104, 16);
+            this.stepLabel.TabIndex = 11;
+            this.stepLabel.Text = "Шаг алгоритма:";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.titleLabel.ForeColor = System.Drawing.Color.DarkBlue;
+            this.titleLabel.Location = new System.Drawing.Point(12, 45);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(428, 25);
+            this.titleLabel.TabIndex = 12;
+            this.titleLabel.Text = "Задание 2 - Генерация горного массива";
             // 
             // Form2
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Name = "Form2";
-            Text = "Form2";
-            ResumeLayout(false);
-            PerformLayout();
+            this.ClientSize = new System.Drawing.Size(800, 520);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.stepLabel);
+            this.Controls.Add(this.seedLabel);
+            this.Controls.Add(this.sizeLabel);
+            this.Controls.Add(this.snowLevelLabel);
+            this.Controls.Add(this.roughnessLabel);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.stepComboBox);
+            this.Controls.Add(this.seedNumeric);
+            this.Controls.Add(this.sizeTrackBar);
+            this.Controls.Add(this.snowLevelTrackBar);
+            this.Controls.Add(this.roughnessTrackBar);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Задание 2 - Генерация горного массива";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roughnessTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.snowLevelTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sizeTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seedNumeric)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TrackBar roughnessTrackBar;
+        private System.Windows.Forms.TrackBar snowLevelTrackBar;
+        private System.Windows.Forms.TrackBar sizeTrackBar;
+        private System.Windows.Forms.NumericUpDown seedNumeric;
+        private System.Windows.Forms.ComboBox stepComboBox;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Label roughnessLabel;
+        private System.Windows.Forms.Label snowLevelLabel;
+        private System.Windows.Forms.Label sizeLabel;
+        private System.Windows.Forms.Label seedLabel;
+        private System.Windows.Forms.Label stepLabel;
+        private System.Windows.Forms.Label titleLabel;
     }
 }
--- Lab5/Lab5/Form2.cs
+++ Lab5/Lab5/Form2.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,98 @@ namespace Lab5
 {
     public partial class Form2: SwitchableForm
     {
+        private MidpointDisplacement generator;
+        private List<Bitmap> stepBitmaps = new List<Bitmap>();
+
         public Form2()
         {
             InitializeComponent();
+            InitializeEvents();
+            GenerateTerrain();
+        }
+
+        private void InitializeEvents()
+        {
+            roughnessTrackBar.Scroll += (s, e) => UpdateLabels();
+            snowLevelTrackBar.Scroll += (s, e) => UpdateLabels();
+            sizeTrackBar.Scroll += (s, e) => UpdateLabels();
+
+            generateButton.Click += (s, e) => GenerateTerrain();
+            stepComboBox.SelectedIndexChanged += (s, e) => ShowSelectedStep();
+
+            UpdateLabels();
+        }
+
+        private void UpdateLabels()
+        {
+            roughnessLabel.Text = $"Шероховатость: {roughnessTrackBar.Value / 100.0:F2}";
+            snowLevelLabel.Text = $"Уровень снега: {snowLevelTrackBar.Value / 100.0:F2}";
+
+            int size = (int)Math.Pow(2, sizeTrackBar.Value) + 1;
+            sizeLabel.Text = $"Размер: {size}x{size}";
+        }
+
+        private void GenerateTerrain()
+        {
+            try
+            {
+                Cursor = Cursors.WaitCursor;
+                generateButton.Enabled = false;
+
+                int size = (int)Math.Pow(2, sizeTrackBar.Value) + 1;
+                double roughness = roughnessTrackBar.Value / 100.0;
+                int seed = (int)seedNumeric.Value;
+                double snowLevel = snowLevelTrackBar.Value / 100.0;
+
+                generator = new MidpointDisplacement(size, roughness, seed);
+                generator.Generate();
+
+                Bitmap bitmap = generator.GenerateBitmap(500, 400, snowLevel);
+
+                stepBitmaps.Clear();
+                stepBitmaps.Add(bitmap);
+                UpdateStepsComboBox();
+                ShowStep(0);
+
+                titleLabel.Text = $"Задание 2 - Горный массив {size}px";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка генерации: {ex.Message}", "Ошибка",
+                              MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                Cursor = Cursors.Default;
+                generateButton.Enabled = true;
+            }
+        }
+
+        private void UpdateStepsComboBox()
+        {
+            stepComboBox.Items.Clear();
+            stepComboBox.Items.Add("Горный массив");
+            stepComboBox.SelectedIndex = 0;
+        }
+
+        private void ShowSelectedStep()
+        {
+            if (stepComboBox.SelectedIndex >= 0)
+                ShowStep(stepComboBox.SelectedIndex);
+        }
+
+        private void ShowStep(int stepIndex)
+        {
+            if (stepIndex >= 0 && stepIndex < stepBitmaps.Count)
+                pictureBox.Image = stepBitmaps[stepIndex];
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            foreach (var bitmap in stepBitmaps)
+                bitmap?.Dispose();
+            stepBitmaps.Clear();
+            base.OnFormClosing(e);
         }
     }
 }
