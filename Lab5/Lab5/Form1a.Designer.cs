e9532e49f3c49f1319adca0194a8bbf9043c8798
WarLockeR31 <roman.borisenko2016@gmail.com>
2025-10-21 22:23:27 +0300

    feat: add tree rendering mode and update assets

--- Lab5/Lab5/Form1a.Designer.cs
+++ Lab5/Lab5/Form1a.Designer.cs
@@ -22,152 +22,168 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            _canvas = new PictureBox();
-            _btnOpen = new Button();
-            _btnRender = new Button();
-            _btnRandomizeSeed = new Button();
-            _numIter = new NumericUpDown();
-            label1 = new Label();
-            label2 = new Label();
-            _numJitter = new NumericUpDown();
-            ((System.ComponentModel.ISupportInitialize)_canvas).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)_numIter).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)_numJitter).BeginInit();
-            SuspendLayout();
-            // 
-            // lblTask
-            // 
-            lblTask.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            lblTask.Location = new Point(644, 17);
-            lblTask.Size = new Size(117, 27);
-            lblTask.Text = "Задание 1а";
-            // 
-            // _canvas
-            // 
-            _canvas.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            _canvas.BorderStyle = BorderStyle.FixedSingle;
-            _canvas.Location = new Point(12, 58);
-            _canvas.Name = "_canvas";
-            _canvas.Size = new Size(605, 380);
-            _canvas.TabIndex = 5;
-            _canvas.TabStop = false;
-            // 
-            // _btnOpen
-            // 
-            _btnOpen.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            _btnOpen.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            _btnOpen.Location = new Point(623, 53);
-            _btnOpen.Name = "_btnOpen";
-            _btnOpen.Size = new Size(79, 30);
-            _btnOpen.TabIndex = 6;
-            _btnOpen.Text = "Open";
-            _btnOpen.UseVisualStyleBackColor = true;
-            _btnOpen.Click += OnOpenClick;
-            // 
-            // _btnRender
-            // 
-            _btnRender.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            _btnRender.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            _btnRender.Location = new Point(708, 53);
-            _btnRender.Name = "_btnRender";
-            _btnRender.Size = new Size(79, 30);
-            _btnRender.TabIndex = 7;
-            _btnRender.Text = "Render";
-            _btnRender.UseVisualStyleBackColor = true;
-            _btnRender.Click += OnRenderClick;
-            // 
-            // _btnRandomizeSeed
-            // 
-            _btnRandomizeSeed.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            _btnRandomizeSeed.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            _btnRandomizeSeed.Location = new Point(623, 89);
-            _btnRandomizeSeed.Name = "_btnRandomizeSeed";
-            _btnRandomizeSeed.Size = new Size(164, 30);
-            _btnRandomizeSeed.TabIndex = 8;
-            _btnRandomizeSeed.Text = "Random seed";
-            _btnRandomizeSeed.UseVisualStyleBackColor = true;
-            _btnRandomizeSeed.Click += _btnRandomizeSeed_Click;
-            // 
-            // _numIter
-            // 
-            _numIter.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            _numIter.Location = new Point(737, 130);
-            _numIter.Maximum = new decimal(new int[] { 50, 0, 0, 0 });
-            _numIter.Name = "_numIter";
-            _numIter.Size = new Size(52, 23);
-            _numIter.TabIndex = 9;
-            _numIter.Value = new decimal(new int[] { 5, 0, 0, 0 });
-            // 
-            // label1
-            // 
-            label1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            label1.Location = new Point(625, 132);
-            label1.Name = "label1";
-            label1.Size = new Size(87, 21);
-            label1.TabIndex = 10;
-            label1.Text = "Iterations:";
-            // 
-            // label2
-            // 
-            label2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            label2.Location = new Point(625, 167);
-            label2.Name = "label2";
-            label2.Size = new Size(101, 21);
-            label2.TabIndex = 12;
-            label2.Text = "Angle jitter:";
-            // 
-            // _numJitter
-            // 
-            _numJitter.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            _numJitter.Location = new Point(737, 165);
-            _numJitter.Maximum = new decimal(new int[] { 90, 0, 0, 0 });
-            _numJitter.Name = "_numJitter";
-            _numJitter.Size = new Size(52, 23);
-            _numJitter.TabIndex = 11;
-            // 
-            // Form1a
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(799, 450);
-            Controls.Add(label2);
-            Controls.Add(_numJitter);
-            Controls.Add(label1);
-            Controls.Add(_numIter);
-            Controls.Add(_btnRandomizeSeed);
-            Controls.Add(_btnRender);
-            Controls.Add(_btnOpen);
-            Controls.Add(_canvas);
-            DoubleBuffered = true;
-            Name = "Form1a";
-            Text = "Form1";
-            Controls.SetChildIndex(lblTask, 0);
-            Controls.SetChildIndex(_canvas, 0);
-            Controls.SetChildIndex(_btnOpen, 0);
-            Controls.SetChildIndex(_btnRender, 0);
-            Controls.SetChildIndex(_btnRandomizeSeed, 0);
-            Controls.SetChildIndex(_numIter, 0);
-            Controls.SetChildIndex(label1, 0);
-            Controls.SetChildIndex(_numJitter, 0);
-            Controls.SetChildIndex(label2, 0);
-            ((System.ComponentModel.ISupportInitialize)_canvas).EndInit();
-            ((System.ComponentModel.ISupportInitialize)_numIter).EndInit();
-            ((System.ComponentModel.ISupportInitialize)_numJitter).EndInit();
-            ResumeLayout(false);
-            PerformLayout();
-        }
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			_canvas = new System.Windows.Forms.PictureBox();
+			_btnOpen = new System.Windows.Forms.Button();
+			_btnRender = new System.Windows.Forms.Button();
+			_btnRandomizeSeed = new System.Windows.Forms.Button();
+			_numIter = new System.Windows.Forms.NumericUpDown();
+			label1 = new System.Windows.Forms.Label();
+			label2 = new System.Windows.Forms.Label();
+			_numJitter = new System.Windows.Forms.NumericUpDown();
+			_btnRenderTree = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)_canvas).BeginInit();
+			((System.ComponentModel.ISupportInitialize)_numIter).BeginInit();
+			((System.ComponentModel.ISupportInitialize)_numJitter).BeginInit();
+			SuspendLayout();
+			// 
+			// lblTask
+			// 
+			lblTask.Anchor = ((System.Windows.Forms.AnchorStyles)(System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right));
+			lblTask.Location = new System.Drawing.Point(643, 17);
+			lblTask.Size = new System.Drawing.Size(117, 27);
+			lblTask.Text = "Задание 1а";
+			// 
+			// _canvas
+			// 
+			_canvas.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right));
+			_canvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			_canvas.Location = new System.Drawing.Point(12, 58);
+			_canvas.Name = "_canvas";
+			_canvas.Size = new System.Drawing.Size(605, 380);
+			_canvas.TabIndex = 5;
+			_canvas.TabStop = false;
+			// 
+			// _btnOpen
+			// 
+			_btnOpen.Anchor = ((System.Windows.Forms.AnchorStyles)(System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right));
+			_btnOpen.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+			_btnOpen.Location = new System.Drawing.Point(623, 53);
+			_btnOpen.Name = "_btnOpen";
+			_btnOpen.Size = new System.Drawing.Size(79, 30);
+			_btnOpen.TabIndex = 6;
+			_btnOpen.Text = "Open";
+			_btnOpen.UseVisualStyleBackColor = true;
+			_btnOpen.Click += OnOpenClick;
+			// 
+			// _btnRender
+			// 
+			_btnRender.Anchor = ((System.Windows.Forms.AnchorStyles)(System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right));
+			_btnRender.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+			_btnRender.Location = new System.Drawing.Point(708, 53);
+			_btnRender.Name = "_btnRender";
+			_btnRender.Size = new System.Drawing.Size(79, 30);
+			_btnRender.TabIndex = 7;
+			_btnRender.Text = "Render";
+			_btnRender.UseVisualStyleBackColor = true;
+			_btnRender.Click += OnRenderClick;
+			// 
+			// _btnRandomizeSeed
+			// 
+			_btnRandomizeSeed.Anchor = ((System.Windows.Forms.AnchorStyles)(System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right));
+			_btnRandomizeSeed.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+			_btnRandomizeSeed.Location = new System.Drawing.Point(623, 89);
+			_btnRandomizeSeed.Name = "_btnRandomizeSeed";
+			_btnRandomizeSeed.Size = new System.Drawing.Size(164, 30);
+			_btnRandomizeSeed.TabIndex = 8;
+			_btnRandomizeSeed.Text = "Random seed";
+			_btnRandomizeSeed.UseVisualStyleBackColor = true;
+			_btnRandomizeSeed.Click += _btnRandomizeSeed_Click;
+			// 
+			// _numIter
+			// 
+			_numIter.Anchor = ((System.Windows.Forms.AnchorStyles)(System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right));
+			_numIter.Location = new System.Drawing.Point(737, 130);
+			_numIter.Maximum = new decimal(new int[] { 50, 0, 0, 0 });
+			_numIter.Name = "_numIter";
+			_numIter.Size = new System.Drawing.Size(52, 23);
+			_numIter.TabIndex = 9;
+			_numIter.Value = new decimal(new int[] { 5, 0, 0, 0 });
+			// 
+			// label1
+			// 
+			label1.Anchor = ((System.Windows.Forms.AnchorStyles)(System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right));
+			label1.AutoSize = true;
+			label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+			label1.Location = new System.Drawing.Point(625, 132);
+			label1.Name = "label1";
+			label1.Size = new System.Drawing.Size(87, 21);
+			label1.TabIndex = 10;
+			label1.Text = "Iterations:";
+			// 
+			// label2
+			// 
+			label2.Anchor = ((System.Windows.Forms.AnchorStyles)(System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right));
+			label2.AutoSize = true;
+			label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+			label2.Location = new System.Drawing.Point(625, 167);
+			label2.Name = "label2";
+			label2.Size = new System.Drawing.Size(101, 21);
+			label2.TabIndex = 12;
+			label2.Text = "Angle jitter:";
+			// 
+			// _numJitter
+			// 
+			_numJitter.Anchor = ((System.Windows.Forms.AnchorStyles)(System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right));
+			_numJitter.Location = new System.Drawing.Point(737, 165);
+			_numJitter.Maximum = new decimal(new int[] { 90, 0, 0, 0 });
+			_numJitter.Name = "_numJitter";
+			_numJitter.Size = new System.Drawing.Size(52, 23);
+			_numJitter.TabIndex = 11;
+			// 
+			// _btnRenderTree
+			// 
+			_btnRenderTree.Anchor = ((System.Windows.Forms.AnchorStyles)(System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right));
+			_btnRenderTree.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+			_btnRenderTree.Location = new System.Drawing.Point(625, 201);
+			_btnRenderTree.Name = "_btnRenderTree";
+			_btnRenderTree.Size = new System.Drawing.Size(162, 30);
+			_btnRenderTree.TabIndex = 13;
+			_btnRenderTree.Text = "RenderTree";
+			_btnRenderTree.UseVisualStyleBackColor = true;
+			_btnRenderTree.Click += OnRenderTreeClick;
+			// 
+			// Form1a
+			// 
+			AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			ClientSize = new System.Drawing.Size(799, 450);
+			Controls.Add(_btnRenderTree);
+			Controls.Add(label2);
+			Controls.Add(_numJitter);
+			Controls.Add(label1);
+			Controls.Add(_numIter);
+			Controls.Add(_btnRandomizeSeed);
+			Controls.Add(_btnRender);
+			Controls.Add(_btnOpen);
+			Controls.Add(_canvas);
+			DoubleBuffered = true;
+			Text = "Form1";
+			Controls.SetChildIndex(lblTask, 0);
+			Controls.SetChildIndex(_canvas, 0);
+			Controls.SetChildIndex(_btnOpen, 0);
+			Controls.SetChildIndex(_btnRender, 0);
+			Controls.SetChildIndex(_btnRandomizeSeed, 0);
+			Controls.SetChildIndex(_numIter, 0);
+			Controls.SetChildIndex(label1, 0);
+			Controls.SetChildIndex(_numJitter, 0);
+			Controls.SetChildIndex(label2, 0);
+			Controls.SetChildIndex(_btnRenderTree, 0);
+			((System.ComponentModel.ISupportInitialize)_canvas).EndInit();
+			((System.ComponentModel.ISupportInitialize)_numIter).EndInit();
+			((System.ComponentModel.ISupportInitialize)_numJitter).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        #endregion
+		private System.Windows.Forms.Button _btnRenderTree;
+
+		#endregion
 
         private PictureBox _canvas;
         private Button _btnOpen;
--- Lab5/Lab5/Form1a.cs
+++ Lab5/Lab5/Form1a.cs
@@ -13,20 +13,29 @@ namespace Lab5
 {
     public partial class Form1a : SwitchableForm
     {
-
         private LSystemDefinition _def;
         private string _lastExpanded = "";
-
+		private bool isTree;
         private int seed;
 
         public Form1a()
         {
             InitializeComponent();
 
-            Resize += (s, e) => { if (_def != null && _lastExpanded != null) Render(); };
+            Resize += (s, e) =>
+			{
+				if (_def != null && _lastExpanded != null)
+				{
+					if (isTree) 
+						RenderTree();
+					else
+						Render();	
+				} 
+			};
         }
 
         private void OnRenderClick(object sender, EventArgs e) => Render();
+		private void OnRenderTreeClick(object sender, EventArgs e) => RenderTree();
 
         private void OnOpenClick(object sender, EventArgs e)
         {
@@ -54,6 +63,7 @@ namespace Lab5
         private void Render()
         {
             if (_def == null) return;
+			isTree = false;
 
             // Параметры
             var iterations = (int)_numIter.Value;
@@ -72,6 +82,10 @@ namespace Lab5
                 _canvas.Image = new Bitmap(_canvas.Width, _canvas.Height);
                 return;
             }
+			
+			int maxDepth = 0;
+			foreach (var s in segments)
+				if (s.Depth > maxDepth) maxDepth = s.Depth;
 
             // 3) Получим все точки (для масштабирования)
             var allPts = new List<Vec2>(segments.Count * 2);
@@ -103,6 +117,89 @@ namespace Lab5
         {
             seed = Environment.TickCount;
         }
+
+		private void RenderTree()
+		{
+			if (_def == null) return;
+			isTree = true;
+			
+			// Параметры
+			var iterations = (int)_numIter.Value;
+			var stepPx = 1f;
+			var jitter = (double)_numJitter.Value; // ± в градусах
+			var rnd = new Random(seed);
+
+			// 1) Развёртка
+			_lastExpanded = LSystemEngine.Expand(_def.Axiom, _def.Rules, iterations, rnd);
+
+			// 2) Трассировка «черепашкой» в единичных шагах
+			var segments = LSystemEngine.Trace(
+				_lastExpanded, 
+				1.0, 
+				_def.TurnAngleDeg, 
+				_def.InitialDirDeg, 
+				jitter, 
+				rnd,
+				treeMode: true
+			);
+			
+			if (segments.Count == 0)
+			{
+				_canvas.Image = new Bitmap(_canvas.Width, _canvas.Height);
+				return;
+			}
+			
+			int maxDepth = 0;
+			foreach (var s in segments)
+				if (s.Depth > maxDepth) maxDepth = s.Depth;
+
+			// 3) Получим все точки (для масштабирования)
+			var allPts = new List<Vec2>(segments.Count * 2);
+			foreach (var s in segments) { allPts.Add(s.A); allPts.Add(s.B); }
+
+			// 4) Масштабирование под PictureBox
+			FitScaler.FitToTarget(allPts, _canvas.ClientSize, out var toScreen, out var bounds);
+
+			// 5) Рисование в Bitmap с помощью FastBitmap
+			var bmp = new Bitmap(Math.Max(1, _canvas.Width), Math.Max(1, _canvas.Height));
+			using (var g = Graphics.FromImage(bmp)) g.Clear(Color.White);
+
+			using (var fb = new FastBitmap.FastBitmap(bmp))
+			{
+				int baseThickness = 10; // напр., 8–14
+				Color brown = Color.FromArgb(139, 69, 19);   // SaddleBrown
+				Color green = Color.FromArgb(34, 139, 34);   // ForestGreen
+
+				foreach (var s in segments)
+				{
+					if (!s.Draw) continue;
+					var p0 = toScreen(s.A);
+					var p1 = toScreen(s.B);
+
+					// Нормированная глубина [0..1]: 0 — ствол, 1 — тонкие ветви
+					double t = (maxDepth > 0) ? (double)s.Depth / maxDepth : 0.0;
+
+					// Толщина: экспоненциальное сужение
+					int thickness = Math.Max(1, (int)Math.Round((Math.Pow(1-t, 3)) * baseThickness));
+
+					// Цвет между коричневым и зелёным
+					var col = LerpColor(brown, green, t);
+
+					FastDraw.DrawLineThick(fb, p0.X, p0.Y, p1.X, p1.Y, thickness, col);
+				}
+			}
+
+			_canvas.Image = bmp;
+		}
+		
+		static Color LerpColor(Color a, Color b, double t)
+		{
+			if (t < 0) t = 0; if (t > 1) t = 1;
+			int r = (int)Math.Round(a.R + (b.R - a.R) * t);
+			int g = (int)Math.Round(a.G + (b.G - a.G) * t);
+			int bch = (int)Math.Round(a.B + (b.B - a.B) * t);
+			return Color.FromArgb(r, g, bch);
+		}
     }
 
 
@@ -217,11 +314,26 @@ namespace Lab5
 
         public readonly struct Segment
         {
-            public readonly Vec2 A, B; public readonly bool Draw;
-            public Segment(in Vec2 a, in Vec2 b, bool draw) { A = a; B = b; Draw = draw; }
+            public readonly Vec2 A, B;
+			public readonly bool Draw;
+			public readonly int Depth;
+
+			public Segment(in Vec2 a, in Vec2 b, bool draw, int depth)
+			{
+				A = a; 
+				B = b; 
+				Draw = draw;
+				Depth = depth;
+			}
         }
 
-        public static List<Segment> Trace(string sequence, double stepLen, double angleDeg, double initialDirDeg, double jitterDeg, Random rng)
+        public static List<Segment> Trace(string sequence, 
+										  double stepLen, 
+										  double angleDeg, 
+										  double initialDirDeg, 
+										  double jitterDeg, 
+										  Random rng,
+										  bool treeMode = false)
         {
             var segs = new List<Segment>(Math.Max(16, sequence?.Length ?? 0));
             if (string.IsNullOrEmpty(sequence)) return segs;
@@ -234,24 +346,20 @@ namespace Lab5
             {
                 switch (ch)
                 {
-                    case 'F':
-                    case 'G':
-                    case 'H': // как F
-                    {
-                        var rad = Deg2Rad(dir);
-                        var delta = new Vec2(Math.Cos(rad) * stepLen, Math.Sin(rad) * stepLen);
-                        var next = new Vec2(pos.X + delta.X, pos.Y + delta.Y);
-                        segs.Add(new Segment(pos, next, draw: true));
-                        pos = next;
-                        break;
-                    }
-                    case 'f': // шаг без рисования
-                    {
-                        var rad = Deg2Rad(dir);
-                        var delta = new Vec2(Math.Cos(rad) * stepLen, Math.Sin(rad) * stepLen);
-                        pos = new Vec2(pos.X + delta.X, pos.Y + delta.Y);
-                        break;
-                    }
+					case 'H':
+					case 'F':
+					{
+						var depth = stack.Count;
+						double stepHere = treeMode ? stepLen * Math.Pow(0.9f, depth) : stepLen;
+
+						var rad = Deg2Rad(dir);
+						var delta = new Vec2(Math.Cos(rad) * stepHere, Math.Sin(rad) * stepHere);
+						var next = new Vec2(pos.X + delta.X, pos.Y + delta.Y);
+
+						segs.Add(new Segment(pos, next, draw: true, depth: depth));
+						pos = next;
+						break;
+					}
                     case '+':
                         dir += angleDeg + (jitterDeg > 0 ? RandSymmetric(rng, jitterDeg) : 0);
                         break;
@@ -268,7 +376,6 @@ namespace Lab5
                         if (stack.Count > 0) { var s = stack.Pop(); pos = s.pos; dir = s.dir; }
                         break;
                     default:
-                        // Прочие символы (X и т.п.) — просто маркеры, игнорируем на этапе рисования
                         break;
                 }
             }
@@ -338,5 +445,43 @@ namespace Lab5
                 if (e2 <= dx) { err += dx; y += sy; }
             }
         }
+		
+		public static void DrawLineThick(FastBitmap.FastBitmap fb, int x0, int y0, int x1, int y1, int thickness, Color color)
+		{
+			if (thickness <= 1) { DrawLine(fb, x0, y0, x1, y1, color); return; }
+			int r = Math.Max(1, thickness / 2);
+
+			int dx = Math.Abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
+			int dy = -Math.Abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
+			int err = dx + dy, e2;
+			int x = x0, y = y0;
+
+			while (true)
+			{
+				FillDisc(fb, x, y, r, color);
+				if (x == x1 && y == y1) break;
+				e2 = 2 * err;
+				if (e2 >= dy) { err += dy; x += sx; }
+				if (e2 <= dx) { err += dx; y += sy; }
+			}
+		}
+
+		private static void FillDisc(FastBitmap.FastBitmap fb, int cx, int cy, int r, Color color)
+		{
+			int r2 = r * r;
+			for (int dy = -r; dy <= r; dy++)
+			{
+				int yy = cy + dy;
+				if ((uint)yy >= (uint)fb.Height) continue;
+				
+				int dxlim = (int)Math.Floor(Math.Sqrt(r2 - dy * dy));
+				for (int dx = -dxlim; dx <= dxlim; dx++)
+				{
+					int xx = cx + dx;
+					if ((uint)xx >= (uint)fb.Width) continue;
+					fb[xx, yy] = color;
+				}
+			}
+		}
     }
 }
